3a9910da9a051518d71917c28f2d95961fbbc703
Aleksandr Zykov <alexandrz@gmail.com>
2012-09-21 18:51:02 -0500

    iframe protection disabled

--- mitsubishi.rb
+++ mitsubishi.rb
@@ -2,14 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'erb'
 
+set :protection, :except => :frame_options
+
 get '/' do
   redirect 'https://www.facebook.com/crazyjapan', 303
 end
 
 post '/tab/' do
-  erb :tab
-end
-
-get '/tab/' do
   erb :tab
 end
